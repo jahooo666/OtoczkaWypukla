2b5645be6e99143cdb52c495c16f3c26cb9f6d72
jahooo666 <okraskaj@ee.pw.edu.pl>
2015-12-25 23:00:58 +0100

    Sprawdzanie jak działa ArrayList >ciekawych rzeczy sie dowiaduje po pol roku uzywania >jak sie dodana konkretne miejsce to przesuwa wszystko w prawo! -WOW > w tym momencie pasuje

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,44 +8,71 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
 
+import java.rmi.MarshalException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Main extends Application {
     ArrayList<Point2D> punkty = new ArrayList<>();
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Otoczka wypukła");
         Group root = new Group();
         Canvas canvas = new Canvas(700, 500);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         dodajPunkty();
-        rysujPunkty(punkty,gc);
+        sortujPunkty();
         root.getChildren().add(canvas);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        rysujPunkty(punkty, gc);
     }
 
-
-    public void findLowestPoint(){
-       //znajduje najniższy punkt
-        Point2D lowest= punkty.get(0);
-        for (Point2D punkt: punkty) {
-            if((punkt.getY()>lowest.getY())
-                    ||((punkt.getY()==lowest.getY())&&(punkt.getY()==lowest.getY()))){
+    public Point2D findLowestPoint() {
+        //znajduje najniższy punkt
+        Point2D lowest = punkty.get(0);
+        for (Point2D punkt : punkty) {
+            if ((punkt.getY() > lowest.getY())
+                    || ((punkt.getY() == lowest.getY()) && (punkt.getY() == lowest.getY()))) {
                 lowest = punkt;
             }
         }
+        return lowest;
     }
 
+    public void sortujPunkty() {
+        Point2D lowest = findLowestPoint();
+        wypiszPunkty();
+        Collections.sort(punkty, new Comparator<Point2D>() {
+            @Override
+            public int compare(Point2D o1, Point2D o2) {
+                double tan1 = Math.atan2(o1.getX(), o1.getY());
+                double tan2 = Math.atan2(o2.getX(), o2.getY());
+                return Double.compare(tan1, tan2);
+            }
+        });
+        System.out.println("nowe");
+        wypiszPunkty();
+    }
 
-    public void dodajPunkty(){
+    public void wypiszPunkty(){
+        for (Point2D punkt: punkty) {
+            System.out.println(punkt);
+        }
+    }
+
+    public void dodajPunkty() {
         this.punkty = new PointsFromFileReader().read("dane.txt");
     }
 
-    public void rysujPunkty(ArrayList<Point2D> punkty,GraphicsContext gc){
-        for(Point2D punkt : punkty){
-            gc.fillOval(punkt.getX(),punkt.getY(),3,3);
+    public void rysujPunkty(ArrayList<Point2D> punkty, GraphicsContext gc) {
+
+        for (Point2D punkt : punkty) {
+            gc.fillOval(punkt.getX(), punkt.getY(), 5, 5);
+            gc.fillText(Integer.toString(punkty.indexOf(punkt)),punkt.getX(), punkt.getY());
         }
     }
 
