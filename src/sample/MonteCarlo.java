e3a2f94eb2b8f3b780d192a932a3888f096d9d23
jahooo666 <okraskaj@ee.pw.edu.pl>
2015-12-26 16:03:36 +0100

    > sprawdzanie czy należy- teraz sie kompiluje- nie wiem czy dziala

--- src/sample/MonteCarlo.java
+++ src/sample/MonteCarlo.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Stack;
 
+import static sample.PrzecinanieOdcinkow.sprawdzPrzecinanie;
+
 public class MonteCarlo {
     public static ArrayList<Point2D> losujPunkty(int ile, Canvas canvas) {
         //losowanie podanej liczby punktow w obszarze ograniczonym przez canvas
@@ -23,27 +25,40 @@ public class MonteCarlo {
         return wylosowane;
     }
 
-    public boolean czyNalezy(Point2D punkt, ArrayList<Point2D> otoczka){
+    public boolean czyNalezy(Point2D punkt, ArrayList<Point2D> otoczka) {
         int liczbaPrzeciec = 0;
+        Point2D punktgora = new Point2D(punkt.getX(),0);
         Point2D firstOfAll = otoczka.get(0);
         Point2D first = firstOfAll;
         Point2D second = otoczka.get(1);
 
-        for (int i = 2; i < otoczka.size() ; i++) {
+        for (int i = 2; i < otoczka.size(); i++) {
             first = second;
             second = otoczka.get(i);
-
-
+            if(sprawdzPrzecinanie(first,second,punkt,punktgora)){
+                liczbaPrzeciec++;
+            }
         }
-        if(liczbaPrzeciec == 1)  return true;
-        if((liczbaPrzeciec == 2)||(liczbaPrzeciec==0))return false;
+        //na koniec ostatni z pierwszym
+        first = second;
+        second = firstOfAll;
+        if(sprawdzPrzecinanie(first,second,punkt,punktgora)){
+            liczbaPrzeciec++;
+        }
+
+        if (liczbaPrzeciec == 1) return true;
+        if ((liczbaPrzeciec == 2) || (liczbaPrzeciec == 0)) return false;
+        else {
+            System.out.println("Jest wiecej niż 2 przeciecia. To jest niemożliwe- otoczka jest źle zrobbiona");
+            return false;
+        }
     }
 
 
     public static ArrayList<Point2D> stackToArrayList(Stack<Point2D> stos) {
         //konwerter stosu na ArrayListe - z odwroconą kolejnością!
         ArrayList<Point2D> wierzcholki = new ArrayList<>();
-        while(!stos.isEmpty()){
+        while (!stos.isEmpty()) {
             wierzcholki.add(stos.pop());
         }
 
